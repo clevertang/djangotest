aa6e9af2c0cb02df961bc6ce98f0924dbd1f83d0
tangxin <961577196@qq.com>
2017-11-27 12:52:31 +0800

    编码

--- app/models.py
+++ app/models.py
@@ -10,12 +10,15 @@ KIND_CHOICES = (
     ("开庭公告", "开庭公告"),
     ("招中标", "招中标"),
 )
-province = ("广东", "广西")
+province = (
+    ("广东", "广东"),
+    ("广西", "广西")
+)
 
 
 class Moment(models.Model):  # orm,对象关系映射
-    入口链接 = models.CharField(max_length=200)
-    站点名称 = models.CharField(max_length=20, default='广东法院网')
-    主题 = models.CharField(max_length=20, choices=KIND_CHOICES, default=KIND_CHOICES[0])
-    省份 = models.CharField(max_length=20, choices=KIND_CHOICES, default=province[0])
-    城市 = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    site_name = models.CharField(max_length=20, default='广东法院网')
+    topic = models.CharField(max_length=20, choices=KIND_CHOICES, default=KIND_CHOICES[0])
+    province = models.CharField(max_length=20, choices=province, default=province[0])
+    city = models.CharField(max_length=200)
